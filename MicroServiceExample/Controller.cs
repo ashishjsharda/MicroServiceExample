40a087bd1d79975d9f5893133bcc6788c41ab52d
Ashish Sharda <ashishjsharda@gmail.com>
2020-03-21 10:07:17 -0400

    Add all my files

---
+++ MicroServiceExample/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+
+namespace MicroServiceExample
+{
+    class Controller :ApiController
+    {
+        [Route("hello")]
+        public String hello()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ MicroServiceExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MicroService4Net; 
+
+namespace MicroServiceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var micro = new MicroService(8082);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
